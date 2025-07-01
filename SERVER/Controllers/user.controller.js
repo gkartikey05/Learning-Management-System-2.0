433cd3b141396c3d4f8275e2ea1ba97618c67b17
gkartikey05 <gkartikey05@gmail.com>
2025-07-01 21:35:07 +0530

    Resolved Bug

--- SERVER/Controllers/user.controller.js
+++ SERVER/Controllers/user.controller.js
@@ -9,7 +9,7 @@ const cookieOptions = {
   secure: process.env.NODE_ENV === "production",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   httpOnly: true,
-  sameSite: "Strict",
+  sameSite: "none",
 };
 
 export const signUp = async (req, res, next) => {
